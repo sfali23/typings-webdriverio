cad520270a4bcf8f70fc713b2c8890a4e52c7535
sfali23 <f.syed.ali@gmail.com>
2016-09-28 15:32:53 -0400

    test cases

---
+++ test/wdio/wdio.conf.ts
@@ -0,0 +1,74 @@
+/// <reference path="./build.d.ts"/>
+
+import {Config} from 'webdriverio';
+
+export var config: Config = <Config>{
+    //
+    // ==================
+    // Specify Test Files
+    // ==================
+    // Define which test specs should run. The pattern is relative to the location of this
+    // file.
+    //
+    specs: [
+        'wdio/test/test.js'
+    ],
+    // Patterns to exclude.
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+    //
+    // ============
+    // Capabilities
+    // ============
+    // Define your capabilities here. WebdriverIO can run multiple capabilties at the same
+    // time. Depending on the number of capabilities WebdriverIO launches several test
+    // sessions. Within your capabilities you can overwrite the spec and exclude option in
+    // order to group specific specs to a specific capability.
+    //
+    // If you have trouble getting all important capabilities together check out Sauce Labs
+    // platform configurator. A great tool to configure your capabilities.
+    // https://docs.saucelabs.com/reference/platforms-configurator
+    //
+    capabilities: [{
+        browserName: 'chrome'
+    }],
+    services: ['selenium-standalone'],
+    //
+    // ===================
+    // Test Configurations
+    // ===================
+    // Define all options that are relevant for the WebdriverIO instance here
+    //
+    // Level of logging verbosity.
+    logLevel: 'silent',
+    //
+    // Enables colors for log output
+    coloredLogs: true,
+    //
+    // Saves a screenshot to a given path if a command fails.
+    screenshotPath: './errorShots/',
+    //
+    // Shorten url command calls by setting a base url. If your url parameter starts with "/"
+    // the base url gets prepended.
+    baseUrl: 'http://localhost',
+    //
+    // Default timeout for all waitForXXX commands.
+    waitforTimeout: 10000,
+    //
+    // Framework you want to run your specs with.
+    // The following are supported: mocha, jasmine and cucumber
+    // see also: http://webdriver.io/guide/testrunner/frameworks.html
+    //
+    // Make sure you have the node package for the specific framework installed before running
+    // any tests. If not please install the following package:
+    // Mocha: `$ npm install mocha`
+    // Jasmine: `$ npm install jasmine`
+    // Cucumber: `$ npm install cucumber`
+    framework: 'mocha',
+    //
+    // Test reporter for stdout.
+    // The following are supported: dot (default), spec and xunit
+    // see also: http://webdriver.io/guide/testrunner/reporters.html
+    reporter: 'dot'
+};
