982e1457c037b6b1ff0a6974b92b3055bb9918f4
sfali23 <f.syed.ali@gmail.com>
2016-09-28 15:30:54 -0400

    added Config interface

--- standalone/index.d.ts
+++ standalone/index.d.ts
@@ -2,6 +2,8 @@ declare namespace WebdriverIO {
 
     export interface Browser<T> {
 
+        options: any;
+
         then<P>(onFulfilled?: (value: T) => P | Browser<P>, onRejected?: (err: any) => P | Browser<P>): Browser<T>;
 
         catch<P>(onRejected?: (err: any) => P | Browser<P>): Browser<P>;
@@ -3177,6 +3179,141 @@ declare namespace WebdriverIO {
 
     export interface WebElementJSONObject {
         ELEMENT: string;
+    }
+
+    export interface Config {
+        host?: string;
+        port?: number;
+        path?: string;
+        user?: string;
+        key?: string;
+        specs?: string[];
+        exclude?: string[];
+        maxInstances?: number;
+        capabilities: Capabilities[];
+        services?: string[];
+        debug?: boolean;
+        execArgv?: string[];
+        sync?: boolean;
+        logLevel?: string;
+        coloredLogs?: boolean;
+        screenshotPath?: string;
+        baseUrl?: string;
+        waitforTimeout?: number;
+        connectionRetryTimeout?: number;
+        connectionRetryCount?: number;
+        plugins?: Plugins;
+        framework?: string;
+        reporter?: string;
+        reporters?: string[];
+        reporterOptions?: ReporterOptions;
+        mochaOpts?: any;
+        jasmineNodeOpts?: any;
+        cucumberOpts?: any;
+        //
+        // =====
+        // Hooks
+        // =====
+        // WebdriverIO provides a several hooks you can use to interfere the test process in order to enhance
+        // it and build services around it. You can either apply a single function to it or an array of
+        // methods. If one of them returns with a promise, WebdriverIO will wait until that promise got
+        // resolved to continue.
+        //
+        /**
+         * Gets executed once before all workers get launched.
+         */
+        onPrepare?: (config?: Config, capabilities?: Capabilities) => void;
+
+        /**
+         * Gets executed before test execution begins. At this point you can access to all global variables like `browser`.
+         *  It is the perfect place to define custom commands.
+         */
+        before?: (capabilities?: Capabilities, specs?: string[]) => void;
+
+        /**
+         * Hook that gets executed before the suite starts
+         */
+        beforeSuite?: (suite?: any) => void;
+
+        /**
+         * Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling beforeEach in Mocha)
+         */
+        beforeHook?: () => void;
+
+        /**
+         * Hook that gets executed _after_ a hook within the suite starts (e.g. runs after calling afterEach in Mocha)
+         */
+        afterHook?: () => void;
+
+        /**
+         * Function to be executed before a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
+         */
+        beforeTest?: (test?: any) => void;
+
+        /**
+         * Runs before a WebdriverIO command gets executed.
+         */
+        beforeCommand?: (commandName: string, ...args: string[]) => void;
+
+        /**
+         * Runs after a WebdriverIO command gets executed
+         */
+        afterCommand?: (commandName: string, args: string[], result?: any, error?: Error) => void;
+
+        /**
+         * Function to be executed after a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
+         */
+        afterTest?: (test?: any) => void;
+
+        /**
+         * Hook that gets executed after the suite has ended
+         */
+        afterSuite?: (suite?: any) => void;
+
+        /**
+         * Gets executed after all tests are done. You still have access to all global variables from the test.
+         */
+        after?: (result?: any, capabilities?: Capabilities, specs?: string[]) => void;
+
+        /**
+         * Gets executed after all workers got shut down and the process is about to exit. It is not possible to defer the end of the process using a promise
+         */
+        onComplete?: (exitCode?: number) => void;
+
+        // Cucumber specific hooks
+        beforeFeature?: (feature?: any) => void;
+        beforeScenario?: (scenario?: any) => void;
+        beforeStep?: (step?: any) => void;
+        afterStep?: (stepResult?: any) => void;
+        afterScenario?: (scenario?: any) => void;
+        afterFeature?: (feature?: any) => void;
+    }
+
+    export interface Capabilities {
+        browserName: string;
+        version?: string;
+        platform?: string;
+        maxInstances?: number;
+        specs?: string[];
+        exclude?: string[];
+    }
+
+    export interface WebdriverCSS {
+        screenshotRoot?: string;
+        failedComparisonsRoot?: string;
+        misMatchTolerance?: number;
+        screenWidth?: number[];
+        updateBaseline?: boolean;
+    }
+
+    export interface Plugins {
+        webdrivercss?: WebdriverCSS;
+        webdriverrtc?: {};
+        browserevent?: {};
+    }
+
+    export interface ReporterOptions {
+        outputDir?: string;
     }
 
     export function remote(options?: {}, modifier?: {}): Browser<any>;
